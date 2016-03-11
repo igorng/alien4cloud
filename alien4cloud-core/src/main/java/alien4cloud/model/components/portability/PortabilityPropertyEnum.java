a4c005d1599b42839549bbc775036f1260fc3769
Xavier DEGENNE <xavier.degenne@fastconnect.fr>
2016-03-11 09:58:23 +0000

    [ALIEN-1440] add a property definition registry for portability

--- alien4cloud-core/src/main/java/alien4cloud/model/components/portability/PortabilityPropertyEnum.java
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/portability/PortabilityPropertyEnum.java
@@ -1,7 +1,7 @@
 package alien4cloud.model.components.portability;
 
 public enum PortabilityPropertyEnum {
-    IAASS("iaaSs"), ORCHESTRATORS("orchestrators");
+    IAASS("iaaSs"), ORCHESTRATORS("orchestrators"), RUNTIME_PACKAGES("runtimePackages");
 
     private String name;
 
@@ -14,7 +14,17 @@ public enum PortabilityPropertyEnum {
         return name;
     }
 
+    public static PortabilityPropertyEnum fromName(String name) {
+        for (PortabilityPropertyEnum e : PortabilityPropertyEnum.values()) {
+            if (e.name.equals(name)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
     public static String IAASS_KEY = IAASS.name;
     public static String ORCHESTRATORS_KEY = ORCHESTRATORS.name;
+    public static String RUNTIME_PACKAGES_KEY = RUNTIME_PACKAGES.name;
 
 }
